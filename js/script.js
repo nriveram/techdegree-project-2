44dc165cc8a9a4386e6566549b27d0464a3970fc
Naomi <47122756+nriveram@users.noreply.github.com>
2023-09-19 12:17:28 -0700

    adding comments

--- js/script.js
+++ js/script.js
@@ -4,17 +4,20 @@ FSJS Project 2 - Data Pagination and Filtering
 */
 
 const studentsPerPage = 9; 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+/**
+ * `showPage` function - This function will create and insert/append 
+ * the elements needed to display a "page" of nine students
+ * @param {list, page} 
+ */
+
+
 function showPage(list, page) {
-  
+   // calculates the start and end indexes 
    const start = (page * studentsPerPage) - studentsPerPage;
    const end = (page * studentsPerPage);
    const studentList = document.querySelector('.student-list'); 
    studentList.innerHTML = '';
-
+   // creates DOM element to display student info 
    for (let i = 0; i < list.length; i++) {
       if (i >= start && i < end) {
          const html = `<li class="student-item cf">
@@ -33,15 +36,18 @@ function showPage(list, page) {
    }
 }
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+/**
+ * `addPagination` function - This function will create and insert/append the 
+ * elements needed for the pagination buttons
+ * @param {list}
+ */
 function addPagination(list) {
+   // calculates the number of pages 
    const numOfButtons = Math.ceil(list.length / studentsPerPage);
    const linkList = document.querySelector('.link-list'); 
    linkList.innerHTML = ''; 
 
+   // loops through the number of pages and adds the DOM elements to the page 
    for (let i = 1; i <= numOfButtons; i++) {
       const html = `
       <li>
@@ -50,16 +56,17 @@ function addPagination(list) {
       linkList.insertAdjacentHTML("beforeend", html);
 
    }
-
-   
+   // adds an active class to the first button 
    linkList.querySelector('button').className = 'active';
 
    linkList.addEventListener('click', (e) => {
       const activeButton = linkList.querySelector('.active');
       const buttonClicked = e.target;
       if (buttonClicked.tagName === 'BUTTON') {
+         // updates active button to button that was clicked by event listener
          activeButton.className = ''; 
          buttonClicked.className = 'active'; 
+         // updates new page with new students
          showPage(list, buttonClicked.innerHTML);
       } 
    });
@@ -81,7 +88,6 @@ function addPagination(list) {
    */
    
 }
-
 
 // Call functions
 addPagination(data);
