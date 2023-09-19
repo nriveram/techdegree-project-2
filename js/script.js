a22b45c7d601e1f761e627be96a1d02585c6cef1
Naomi <47122756+nriveram@users.noreply.github.com>
2023-09-19 14:41:18 -0700

    extra credit + comments

--- js/script.js
+++ js/script.js
@@ -3,19 +3,18 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
+const linkList = document.querySelector('.link-list'); 
+const studentList = document.querySelector('.student-list');
 const studentsPerPage = 9; 
 /**
  * `showPage` function - This function will create and insert/append 
  * the elements needed to display a "page" of nine students
  * @param {list, page} 
  */
-
-
 function showPage(list, page) {
    // calculates the start and end indexes 
    const start = (page * studentsPerPage) - studentsPerPage;
    const end = (page * studentsPerPage);
-   const studentList = document.querySelector('.student-list'); 
    studentList.innerHTML = '';
    // creates DOM element to display student info from list
    for (let i = 0; i < list.length; i++) {
@@ -43,8 +42,7 @@ function showPage(list, page) {
  */
 function addPagination(list) {
    // calculates the number of pages 
-   const numOfButtons = Math.ceil(list.length / studentsPerPage);
-   const linkList = document.querySelector('.link-list'); 
+   const numOfButtons = Math.ceil(list.length / studentsPerPage); 
    linkList.innerHTML = ''; 
 
    // loops through the number of pages and adds the DOM elements to the page 
@@ -89,6 +87,53 @@ function addPagination(list) {
    
 }
 
+/*EXTRA CREDIT*/
+
+/**
+ * `addSearchBar` function - This function will add a search component, 
+ * search functionality, and pagination search results to the browser. 
+ */
+function addSearchBar() {
+   // this function will add the matching elements of the user's input value
+   // to the browser 
+   function searchStudent(element) {
+      const newData = []; 
+      const input = element.value.toLowerCase(); 
+      for (let i = 0; i < data.length; i++ ) {
+         const studentName = data[i].name;
+         const currStudent = `${studentName.first.toLowerCase()} ${studentName.last.toLowerCase()}`;
+         // compare's student names to user's input 
+         if (currStudent.includes(input)) {
+            newData.push(data[i]);
+         }
+      }
+      // checks if the new data has results to display to the browser 
+      if (newData.length > 0) { 
+         addPagination(newData);
+         showPage(newData, 1);
+      } else { 
+         studentList.innerHTML = '<h3>No results found.</h3>'; 
+         linkList.innerHTML = '';
+      }
+   }
+   // creates the DOM element containing the search bar to the header 
+   const header = document.querySelector('.header'); 
+   const html = `<label for="search" class="student-search">
+   <span>Search by name</span>
+   <input id="search" placeholder="Search by name...">
+   <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+ </label>`; 
+   header.insertAdjacentHTML("beforeend", html);
+   // grabs the user's input from bar to search for student in data
+   const searchBarInput = document.querySelector('#search'); 
+   searchBarInput.addEventListener('keyup', () => {
+      searchStudent(searchBarInput);
+   });
+   
+}
+
+ 
 // Call functions
 addPagination(data);
 showPage(data, 1);
+addSearchBar(); 
